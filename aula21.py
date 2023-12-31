b8e740a0b089de09e867446c4b13558ced0b31fb
EdFeitoza <edfeitoza@gmail.com>
2023-12-31 06:53:05 -0300

    new upload

--- aula21.py
+++ aula21.py
@@ -8,13 +8,23 @@ not => todas as condições precisam ser falsas (false)
 
 Falsy => 0 / 0.0 / '' - considerados falsos
 none => não valor
-'''
 
 menu = input('[E]ntrar [S]air: ')
 senha_informada = input('informe a senha: ')
 senha_permitida = 'senha'
 
 if menu == 'E' and senha_informada == senha_permitida:
+    print('Senha correta!')
+else:
+    print('Dados invalidos')
+    
+'''
+
+menu = input('[E]ntrar [S]air: ')
+senha_informada = input('informe a senha: ')
+senha_permitida = 'senha'
+
+if (menu == 'E' or menu == 'e') and senha_informada == senha_permitida:
     print('Senha correta!')
 else:
     print('Dados invalidos')
---
+++ aula23.py
@@ -0,0 +1,21 @@
+'''
+Operador lógico "not"
+Usado para inverter expressões
+not true = False
+note False = true
+
+Operadores in e not in
+Strings são iteráveis
+
+ 0 1 2 3 4 5  => indice
+ P Y T H O N  => String
+-6-5-4-3-2-1  => indice negativo
+
+'''
+nome = 'Python'
+
+#print(nome[2])
+#print(nome[-3])
+
+print('y' in nome)
+print('u' in nome)
