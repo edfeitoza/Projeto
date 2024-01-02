ca3a933d6360b5d7199a97e84c252c8ef4ed7aef
EdFeitoza <edfeitoza@gmail.com>
2024-01-01 21:03:04 -0300

    Vew upload

---
+++ aula36.py
@@ -0,0 +1,16 @@
+'''
+=   -> atribuição
++=  -> atribui com soma
+-=  -> atribui subtraindo
+*=  -> atribui multiplicando
+/=  -> atribui dividindo
+//= -> atribui divisão de inteiro
+**= -> atribui com exponenciação
+%=  -> atribui módulo (resto)
+'''
+
+cont = 0
+
+while cont < 10:
+    cont += 1
+    print(cont*'-', cont)
