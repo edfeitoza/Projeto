0bf878a8837b551c41061902387eb467a289604f
EdFeitoza <edfeitoza@gmail.com>
2024-01-03 07:00:07 -0300

    new upload

--- aula48.py
+++ aula48.py
@@ -5,7 +5,11 @@ Tipo list - mutável
 Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - indices e fatiamento
 Métodos úteis: append, insert, pop, del, clear, extend, +
-'''
+
+CRUD
+Create, Read, Update e Delete => lista[i]
+
+###########################################
 #            01234
 #           -54321
 string =    'ABCDE'
@@ -16,4 +20,20 @@ lista = [123, True, 'Python', 1.4, []]
 print(lista)
 print(lista[3])
 lista[2] = 'Pythonicos'
+print(lista)
+
+###############################################
+lista = [10, 20, 30, 40]
+print(lista)
+lista.append(50)
+lista.append(60)
+lista.append(70)
+print(lista)
+ultimo_Valor = lista.pop(3)
+print(lista, 'Removido: ', ultimo_Valor)
+###############################################
+'''
+lista = [10, 20, 30, 40]
+print(lista)
+lista.insert(0, 1)
 print(lista)
